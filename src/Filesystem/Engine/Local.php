2dbcc9aed9c464799234dea771bf4633f45211bc
Vagelis Papas <papas@source.gr>
2020-11-10 15:08:52 +0200

    New Generic Trait for Drives

--- src/Filesystem/Engine/Local.php
+++ src/Filesystem/Engine/Local.php
@@ -6,9 +6,10 @@
     namespace Sourcegr\Framework\Filesystem\Engine;
 
 
-
     class Local implements FileSystemDriveInterface
     {
+        use GenericDriveTrait;
+
         protected $drivePath;
 
         protected function getRealPath(string $directory = null): string
@@ -18,13 +19,6 @@
         }
 
 
-        /**
-         * FileSystemDrive constructor.
-         *
-         * @param string $drivePath
-         *
-         * @throws \Exception
-         */
         public function __construct(string $drivePath)
         {
             if (!is_dir($drivePath)) {
@@ -34,47 +28,60 @@
             $this->drivePath = $drivePath;
         }
 
-        public function getDrivePath(): string
-        {
-            return $this->drivePath;
-        }
 
         public function isWritable(string $directory = ''): bool
         {
             return is_writable($this->getRealPath($directory));
         }
 
+
         public function isReadable(string $directory = ''): bool
         {
             return is_readable($this->getRealPath($directory));
         }
 
-        public function saveFile(string $filenameWithPath, string $toFilename, string $onDirectory = ''): bool
+
+        public function saveFile(string $filenameWithPath, string $toFilename, string $onDirectory = '')
         {
-            return @copy($filenameWithPath, $this->getRealPath($onDirectory) . $toFilename);
+            if (@copy($filenameWithPath, $this->getRealPath($onDirectory) . $toFilename)) {
+                return $toFilename;
+            }else {
+                return false;
+            }
         }
+
+
+        public function saveFileUniq(string $filenameWithPath, string $toFilename, string $onDirectory = '')
+        {
+            $f = $this->createUniqFilename($toFilename);
+            return $this->saveFile($filenameWithPath, $f, $onDirectory);
+        }
+
 
         public function deleteFile(string $filename, string $onDirectory = ''): bool
         {
             return @unlink($this->getRealPath($onDirectory) . $filename);
         }
 
+
         public function createDirectory(string $directory, bool $recursive = false, int $mode = 0755): bool
         {
             $dir = $this->getRealPath($directory);
-//            die($dir);
             return @mkdir($dir, $mode, $recursive);
         }
+
 
         public function getFileList(string $directory = ''): array
         {
             return array_values(array_filter(glob($this->getRealPath($directory) . '*'), 'is_file'));
         }
 
-            public function getDirectoryList(string $directory = ''): array
+
+        public function getDirectoryList(string $directory = ''): array
         {
             return array_values(array_filter(glob($this->getRealPath($directory) . '*'), 'is_dir'));
         }
+
 
         public function deleteDirectory(string $directory, bool $recursive = false): bool
         {
@@ -82,6 +89,7 @@
             if (!$recursive) {
                 return @rmdir($dir);
             }
+            // todo add recursive delete
         }
 
 
